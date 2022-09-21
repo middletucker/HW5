0d0a28c305ac9179db46e56071306d4b4978835e
middletucker <80505827+middletucker@users.noreply.github.com>
2022-09-21 15:32:35 +0000

    problem 2 comments

--- HW5P2/main2.cpp
+++ HW5P2/main2.cpp
@@ -4,26 +4,36 @@
 using namespace std;
 
 int main() {
+  //Load input file
   ifstream inFile;
   inFile.open("NumberData1.txt");
 
   int dataTemp, sumData = 0, counter = 0;
-
+  
+  //Load in data of input file with while loop
   while (!inFile.eof()) {
     inFile >> dataTemp;
     // cout << dataTemp << endl;
+
+    //Check to see if data is valid
     if (dataTemp >= 0) {
+      //Update the cumulative data
       sumData = sumData + dataTemp;
       counter++;
-    } else {
+      
+    } else { // value is negative so we should exit
       cout << "Negative value encountered of " << dataTemp << endl;
       cout << "Will not use rest of the data... " << endl;
       break;
     }
   }
+  
   cout << "The sum of valid data is; " << sumData << endl;
   cout << "The number of valid data points is: " << counter << endl;
+  
+  //Integer output
   cout << "The average of the valid data is: " << sumData / counter << endl;
+  //Double output
   cout << "The average of the valid data is: "
-       << (static_cast<double>(sumData) / counter) << endl;
+       << (static_cast<double>(sumData) / counter) << endl; 
 }
